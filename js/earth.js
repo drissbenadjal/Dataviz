559e33d5dca618e684f916c63619e6bef680313a
DRISS BENADJAL <driss.benadjal@hotmail.com>
2022-10-10 22:03:03 +0200

    V4 (modification titre, modifications des dates, ajouts d'effets)

--- js/earth.js
+++ js/earth.js
@@ -1,12 +1,13 @@
 function initialize() {
 
+    btn1965 = document.getElementById("btn1965");
+    btn1985 = document.getElementById("btn1985");
+    btn1995 = document.getElementById("btn1995");
+    btn2000 = document.getElementById("btn2000");
+    btn2005 = document.getElementById("btn2005");
+    btn2010 = document.getElementById("btn2010");
     btn2015 = document.getElementById("btn2015");
-    btn2016 = document.getElementById("btn2016");
-    btn2017 = document.getElementById("btn2017");
-    btn2018 = document.getElementById("btn2018");
-    btn2019 = document.getElementById("btn2019");
     btn2020 = document.getElementById("btn2020");
-    btn2021 = document.getElementById("btn2021");
     btn2022 = document.getElementById("btn2022");
 
     localStorage.setItem("year", 2022);
@@ -53,6 +54,48 @@ function initialize() {
     }
     markerGenerate();
 
+    btn1965.addEventListener("click", function () {
+        document.querySelector(".activeyear").classList.remove("activeyear");
+        let year = localStorage.setItem("year", 1965);
+        btn1965.classList.add("activeyear");
+        markerGenerate();
+    });
+
+    btn1985.addEventListener("click", function () {
+        document.querySelector(".activeyear").classList.remove("activeyear");
+        let year = localStorage.setItem("year", 1985);
+        btn1985.classList.add("activeyear");
+        markerGenerate();
+    });
+
+    btn1995.addEventListener("click", function () {
+        document.querySelector(".activeyear").classList.remove("activeyear");
+        let year = localStorage.setItem("year", 1995);
+        btn1995.classList.add("activeyear");
+        markerGenerate();
+    });
+
+    btn2000.addEventListener("click", function () {
+        document.querySelector(".activeyear").classList.remove("activeyear");
+        let year = localStorage.setItem("year", 2000);
+        btn2000.classList.add("activeyear");
+        markerGenerate();
+    });
+
+    btn2005.addEventListener("click", function () {
+        document.querySelector(".activeyear").classList.remove("activeyear");
+        let year = localStorage.setItem("year", 2005);
+        btn2005.classList.add("activeyear");
+        markerGenerate();
+    });
+
+    btn2010.addEventListener("click", function () {
+        document.querySelector(".activeyear").classList.remove("activeyear");
+        let year = localStorage.setItem("year", 2010);
+        btn2010.classList.add("activeyear");
+        markerGenerate();
+    });
+
     btn2015.addEventListener("click", function () {
         document.querySelector(".activeyear").classList.remove("activeyear");
         let year = localStorage.setItem("year", 2015);
@@ -60,45 +103,10 @@ function initialize() {
         markerGenerate();
     });
 
-    btn2016.addEventListener("click", function () {
-        document.querySelector(".activeyear").classList.remove("activeyear");
-        let year = localStorage.setItem("year", 2016);
-        btn2016.classList.add("activeyear");
-        markerGenerate();
-    });
-
-    btn2017.addEventListener("click", function () {
-        document.querySelector(".activeyear").classList.remove("activeyear");
-        let year = localStorage.setItem("year", 2017);
-        btn2017.classList.add("activeyear");
-        markerGenerate();
-    });
-
-    btn2018.addEventListener("click", function () {
-        document.querySelector(".activeyear").classList.remove("activeyear");
-        let year = localStorage.setItem("year", 2018);
-        btn2018.classList.add("activeyear");
-        markerGenerate();
-    });
-
-    btn2019.addEventListener("click", function () {
-        document.querySelector(".activeyear").classList.remove("activeyear");
-        let year = localStorage.setItem("year", 2019);
-        btn2019.classList.add("activeyear");
-        markerGenerate();
-    });
-
     btn2020.addEventListener("click", function () {
         document.querySelector(".activeyear").classList.remove("activeyear");
         let year = localStorage.setItem("year", 2020);
         btn2020.classList.add("activeyear");
-        markerGenerate();
-    });
-
-    btn2021.addEventListener("click", function () {
-        document.querySelector(".activeyear").classList.remove("activeyear");
-        let year = localStorage.setItem("year", 2021);
-        btn2021.classList.add("activeyear");
         markerGenerate();
     });
 
