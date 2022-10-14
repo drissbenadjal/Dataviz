e20928b35f2c65613ae960113adba130d3012366
Chaimabensaber <chaimabensaber92@gmail.com>
2022-10-14 17:34:23 +0200

    V12
    +d3
    Modif data.json
    Modif du style pour intégrer la map d3 + Grid
    Ajout du mapd3.js
    Ajout du pays.json

---
+++ js/mapd3.js
@@ -0,0 +1,29 @@
+// The svg
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+// Map and projection
+var projection = d3.geoMercator()
+    .center([2, 47])                // GPS of location to zoom on
+    .scale(980)                       // This is like the zoom
+    .translate([ width/2, height/2 ])
+
+// Load external data and boot
+d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson", function(data){
+
+    // Filter data
+    data.features = data.features.filter(function(d){console.log(d.properties.name) ; return d.properties.name=="France"})
+
+    // Draw the map
+    svg.append("g")
+        .selectAll("path")
+        .data(data.features)
+        .enter()
+        .append("path")
+          .attr("fill", "#090909")
+          .attr("d", d3.geoPath()
+              .projection(projection)
+          )
+        .style("stroke", "none")
+})
