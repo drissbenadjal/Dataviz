1c6a5094854dcc2e0ffa8b3cf65780a1c37cd96c
DRISS BENADJAL <74878941+drissbenadjal@users.noreply.github.com>
2022-10-16 13:50:27 +0200

    V13
    Modif du Zoom de la planet et de l'aura

--- js/earth.js
+++ js/earth.js
@@ -17,11 +17,11 @@ function initialize() {
     WE.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     }).addTo(earth);
 
-    earth.setView([40, 0], 2.288);
+    earth.setView([40, 0], 2.6);
     earth.setTilt(0);
     earth.setHeading(0);
-    earth.setMinAltitude(7000000);
-    earth.setMaxAltitude(25000000);
+    earth.setMinAltitude(14000000);
+    earth.setMaxAltitude(23000000);
 
 
     console.log(earth.getZoom());
