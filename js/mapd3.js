ce0ee8185adf7cdddf4f00bfb694179b05d12bcb
DRISS BENADJAL <driss.benadjal@hotmail.com>
2022-11-04 23:24:41 +0100

    modif d3 map

--- js/mapd3.js
+++ js/mapd3.js
@@ -1,13 +1,15 @@
 // The svg
 function affichePays(namePays, pays) {
+    d3.selectAll("#my_dataviz g").remove()
     var svg = d3.select("svg"),
         width = +svg.attr("width"),
         height = +svg.attr("height");
-
+       
+        console.log(pays[namePays].scale) 
     // Map and projection
     var projection = d3.geoMercator()
         .center([pays[namePays].long, pays[namePays].lat])                // GPS of location to zoom on
-        .scale(pays[namePays].scale)                       // This is like the zoom
+        .scale(pays[namePays].scale)               // This is like the zoom
         .translate([width / 2, height / 2])
 
     // Load external data and boot
